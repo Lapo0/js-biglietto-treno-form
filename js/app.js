cc5bb3ae1c726a5e82a0c02a7af52180368d7273
Lapo0 <lapo.gini.01@gmail.com>
2023-02-01 18:06:08 +0100

    FEAT js .textContent in HTML

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
 // NOME E COGNOME
 const firstName = document.getElementById('first-name')
 const lastName = document.getElementById('last-name')
-console.log(firstName, lastName)
-
 
 
 // CHILOMETRI DA PERCORRERE
 const distanza = document.querySelector('.container .distanza')
-console.log(distanza)
 
 // AL CLICK DEL MOUSE
 const formBtnElement = document.querySelector('.container input[type="button"]')
@@ -15,12 +12,28 @@ const formBtnElement = document.querySelector('.container input[type="button"]')
 formBtnElement.addEventListener('click', function () {
 console.log('applica modifiche al box')
 
+// Nome e Cognome
+console.log(firstName.value);
+console.log(lastName.value);
+
+const nomeUtenteElement = document.querySelector('.nome-utente');
+nomeUtenteElement.textContent = firstName.value;
+
+const cognomeUtenteElement = document.querySelector('.cognome-utente');
+cognomeUtenteElement.textContent = lastName.value;
+
+// Km
+console.log(distanza.value);
+
+const chilometriElement = document.querySelector('.chilometri-utente');
+chilometriElement.textContent = distanza.value;
+
 
 // PREZZO BASE BIGLIETTO
 const PREZZO_PER_KM = 0.21
 const prezzoBase = PREZZO_PER_KM * distanza.value
 
-console.log(prezzoBase)
+console.log(prezzoBase.toFixed(2))
 
 let sconto = 0
 
@@ -43,9 +56,16 @@ let prezzoFinale = prezzoBase
             break
     }
 
-    console.log(prezzoFinale)
+    console.log(prezzoFinale.toFixed(2))
+
+    const prezzoFinaleElement = document.querySelector('.prezzo-finale');
+    prezzoFinaleElement.textContent = prezzoFinale;
+
+
+    
+    // Mandare a schermo Nome e Cognome
+    
 
 })
 
-// Mandare a schermo Nome e Cognome
 
